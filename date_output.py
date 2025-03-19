e6c1f7db0fd56262a2dc00a705406b080b21b797
IA <158739081+IA-07@users.noreply.github.com>
2025-03-20 00:28:39 +0600

    date_output.py

--- date_output.py
+++ date_output.py
@@ -2,4 +2,24 @@ print("Today is 19th March, 2025")
 Name= "Iftekhar"
 print("Hello!"+Name)
 Name= "Tasnim"
-print("Hi! Tasnim")
+print("Hi!"+Name)
+Item="Using Python as a calculator"
+print("Some mathmetical calculation are given below- "+Item)
+print(4+5)
+print(9*7)
+print(-1/4)
+print(1/3)
+print(((2050/5)-32)/9)
+print(2**10)
+def greet():
+    print("Hello, World!") 
+if True:
+    print("This is a keyword example.")  
+def add(a, b):
+    return a + b  
+print(add(10, 9))  
+#This program adds two numbers
+Num1= 3
+Num2= 2
+Sum= Num1 + Num2
+print("The number of the sum is:",Sum)
